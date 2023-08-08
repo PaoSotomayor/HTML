59ae5ce36938d64b49381d4dc46b950012f807a0
Pao Sotomayor <paolasotomayor@hotmail.com>
2023-08-08 15:02:26 -0500

    Header reutilizable con jQuery

---
+++ ejercicio3y4_extra_JS/index.js
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <script>
+            function load_header() {
+            }            document.getElementById("contenidoHeader").innerHTML='<object type="text/html" data="menu.html" ></object>';
+
+        </script>
+    </head>
+    <header>
+        <div id="contenidoHeader"></div>
+        <script>
+            load_header();
+        </script>
+    </header>
+    <body>
+        <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto tenetur ratione mollitia non at quam assumenda aperiam unde, itaque totam, distinctio iusto. Saepe sint perspiciatis expedita aut nesciunt maiores. Amet!
+        </p>
+    </body>
+</html>
